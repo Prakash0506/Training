2515a007365c6aedfc944a97780f4588d160f24f
Prakash D <Prakash.d@avasoft.com>
2023-04-28 18:40:46 +0530

    dummy

--- app/src/main/java/com/avasoft/gittraining/MainActivity.kt
+++ app/src/main/java/com/avasoft/gittraining/MainActivity.kt
@@ -27,6 +27,8 @@ class MainActivity : ComponentActivity() {
 //                    kjbcdhfbv
 //                    kdnsbcgadev
 //                    bcjwhdv
+//                    ,bjhb
+
                 }
             }
         }
